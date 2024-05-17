412dc69baea77bd4300d4dc44fd7b2d9b19fc969
aliibaker <alibbaker@outlook.com>
2024-05-16 23:24:18 -0700

    Chapter 4.6, longer lexemes

--- java/com/craftinginterpreters/lox/Scanner.java
+++ java/com/craftinginterpreters/lox/Scanner.java
@@ -53,14 +53,77 @@ class Scanner {
       case '<':
         addToken(match('=') ? LESS_EQUAL : EQUAL);
         break;
-      case '>';
+      case '>':
         addToken(match('=') ? GREATER_EQUAL : EQUAL);
         break;
+      case '/':
+        // check if it is a comment
+        if (match('/')) {
+          // Peek the next character until you hit the next line or EOF.
+          // This means the comment is over. 
+          while(peek() != '\n' && !isAtEnd()) advance();
+        }
+        else {
+          addToken(SLASH);
+        }
+        break;
+      case ' ':
+      case '\r':
+      case '\t':
+        // Ignore whitespace
+        break;
+      // Why we use peek() instead of match() when trying
+      // to find \n when parsing a comment lexeme. 
+      case '\n':
+        line++;
+        break;
+
+      // string literal
+      case '"': string(); break;
 
       default: 
-        Lox.error(line, "Unexpected character.");
+        if(isDigit(c)){
+          number();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
+        
         break; 
     }
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
+  private voide number() {
+    while(isDigit(peek())) advance();
+
+    if(peek() == "." && isDigit(peekNext())){
+      advance();
+
+      while(isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start,current)));
+  }
+
+  private void string() {
+    while(peek() != '"' && isAtEnd()){
+      // for multi line strings
+      if (peek() == '\n') line++;
+      advance();
+    }
+
+    if(isAtEnd) {
+      Lox.error(line, "Unterminated string");
+    }
+
+    advance();
+
+    // Minus one and plus one to remove the ""
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
   }
 
   private boolean match(char expected) {
@@ -86,5 +149,13 @@ class Scanner {
 
   private boolean isAtEnd() {
     return current >= source.length();
+  }
+
+  // One character lookahead. Typically
+  // most languages only use peeks of one or two characters
+  // ahead due to performance constrains. 
+  private char peek(){
+    if(isAtEnd()) return '\0';
+    return source.charAt(current);
   }
 }
